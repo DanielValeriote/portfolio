01a01840168a34673abd3d8d5c46e681dac6b621
DanielValeriote <danielvaleriote@outlook.com>
2022-02-22 18:13:19 -0300

    create toggle dropdown script

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
-function dropdownToggle () {
-  document.getElementById("dropdown").classList.toggle("hidden");
-}
+const dropdown = document.getElementById("dropdown");
+const dropdownBtn = document.getElementById("dropdown-btn");
+
+window.onclick = e => {
+  if (e.target !== dropdownBtn) dropdown.classList.add("hidden");
+};
+
+const dropdownToggle = () => dropdown.classList.toggle("hidden");
