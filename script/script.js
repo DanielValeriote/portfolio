07bc0f06e245a2aa1dca54d530c227e49a36179d
DanielValeriote <danielvaleriote@outlook.com>
2022-03-26 21:12:25 -0300

    js code optimizations

--- script/script.js
+++ script/script.js
@@ -1,27 +1,17 @@
 const dropdown = document.getElementById("dropdown");
 const dropdownBtn = document.getElementById("dropdown-btn");
-window.onclick = e => {
-  if (e.target !== dropdownBtn) dropdown.classList.add("hidden");
-};
-const dropdownToggle = () => dropdown.classList.toggle("hidden");
+window.onclick = e => {if (e.target !== dropdownBtn) dropdown.classList.add("hidden")};
+const dropdownToggle = _ => dropdown.classList.toggle("hidden");
 
-const initHiddenElements = document.querySelectorAll(".init-hidden")
-
+const initHiddenEls = document.querySelectorAll(".init-hidden")
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("onscroll-animate"); //, ent.isIntersecting);
+        entry.target.classList.add("onscroll-animate");
         observer.unobserve(entry.target);
       }
-    },
-    {
-      threshold: [0, 0.5, 1],
-    }
+    },{threshold: [0, 0.5, 1]}
   );
 });
-
-initHiddenElements.forEach((e) => {
-  observer.observe(e);
-});
-
-
+let l = initHiddenEls.length;
+for(let i=0;i<=l;i++) observer.observe(initHiddenEls[i]);
