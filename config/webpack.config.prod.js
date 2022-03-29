8b6e5ebac1b21dcdac64d2f838284ee651a59d6d
DanielValeriote <danielvaleriote@outlook.com>
2022-03-29 15:37:30 -0300

    fix double dotted image files

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "bundle.js",
-    assetModuleFilename: "images/[name].[ext]",
+    assetModuleFilename: "images/[name][ext]",
   },
   plugins: [
     new MiniCssExtract(),
