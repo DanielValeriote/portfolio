af82dc419f4123ebf0276ac8a6272c8fb071a971
DanielValeriote <danielvaleriote@outlook.com>
2022-04-27 21:37:29 -0300

    fix performance issue at scrolldown to projects section

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ const dropdownBtn = document.getElementById("dropdown-btn");
 const dropdownToggle = _ => dropdown.classList.toggle("hidden");
 dropdownBtn.addEventListener("click", dropdownToggle);
 window.onclick = e => {if (e.target !== dropdownBtn) dropdown.classList.add("hidden")};
+
 const initHiddenEls = document.querySelectorAll(".init-hidden");
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add("onscroll-animate");
         observer.unobserve(entry.target);
-      }
+      };
     },{threshold: [0, 0.5, 1]}
   );
 });
