c3baa8428b924345e5296a7a36a697cf4269ce3f
DanielValeriote <danielvaleriote@outlook.com>
2022-06-24 10:32:56 -0300

    improve page accessibility

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import "./styles/style.scss";
 
+//Mobile navbar dropdown
+
 const dropdown = document.getElementById("dropdown");
 const dropdownBtn = document.getElementById("dropdown-btn");
-const dropdownToggle = _ => dropdown.classList.toggle("hidden");
+
+const dropdownToggle = _ => {
+  dropdown.classList.toggle("hidden");
+  dropdown.ariaExpanded = dropdown.ariaExpanded == "true" ? "false" : "true"
+  
+  console.log(dropdown.ariaExpanded)
+} 
+
 dropdownBtn.addEventListener("click", dropdownToggle);
 window.onclick = e => {if (e.target !== dropdownBtn) dropdown.classList.add("hidden")};
+
+//On scroll animation
 
 const initHiddenEls = document.querySelectorAll(".init-hidden");
 const observer = new IntersectionObserver((entries) => {
