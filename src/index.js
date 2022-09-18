2264dd9d72cfcd81a4a28a4b9047a18497902fdb
DanielValeriote <danielvaleriote@outlook.com>
2022-09-17 21:50:23 -0300

    change h2

--- src/index.js
+++ src/index.js
@@ -5,24 +5,28 @@ import "./styles/style.scss";
 const dropdown = document.getElementById("dropdown");
 const dropdownBtn = document.getElementById("dropdown-btn");
 
-const dropdownToggle = _ => {
+const dropdownToggle = () => {
   dropdown.classList.toggle("hidden");
   dropdown.ariaExpanded = dropdown.ariaExpanded == "true" ? "false" : "true";
-} 
+};
 
 dropdownBtn.addEventListener("click", dropdownToggle);
-window.onclick = e => {if (e.target !== dropdownBtn) dropdown.classList.add("hidden")};
+window.onclick = (e) => {
+  if (e.target !== dropdownBtn) dropdown.classList.add("hidden");
+};
 
 //On scroll animation
 
 const initHiddenEls = document.querySelectorAll(".init-hidden");
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
+  entries.forEach(
+    (entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("onscroll-animate");
         observer.unobserve(entry.target);
-      };
-    },{threshold: [0, 0.5, 1]}
+      }
+    },
+    { threshold: [0, 0.5, 1] }
   );
 });
-initHiddenEls.forEach(e => observer.observe(e));
+initHiddenEls.forEach((e) => observer.observe(e));
